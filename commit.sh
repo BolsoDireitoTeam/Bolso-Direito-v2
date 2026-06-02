4f69a2e6d75d8a5bd7b33444277133c27a5954c8
MatheusKubrusly <matheusmontenegro100@gmail.com>
2026-06-02 08:58:01 -0300

    Realizando merge com branch "redux-entity-adapter"

--- commit.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-git add .
-git commit -m "refactor: migrar gerenciamento de estado global para Redux Toolkit" -m "- Substituir FinanceContext e useFinance por Redux Toolkit" -m "- Criar store com slices dedicados: finance, metas, investimentos, user, ui" -m "- Migrar todos os componentes e páginas para usar useAppSelector e useAppDispatch" -m "- Implementar middleware de sincronização cross-slice (metas/investimentos -> saldo)" -m "- Remover hooks e provedores de contexto legados"
